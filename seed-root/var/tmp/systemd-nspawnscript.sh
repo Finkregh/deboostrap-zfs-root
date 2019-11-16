63991a3459d31e0831a916ee3bff48b9b82fde9c
Oluf Lorenzen <oluf.lorenzen@b1-systems.de>
2019-11-16 19:25:44 +0100

    properly set ENVVARs

--- seed-root/var/tmp/systemd-nspawnscript.sh
+++ seed-root/var/tmp/systemd-nspawnscript.sh
@@ -4,9 +4,9 @@ declare -r DEST_DISK_ID="__DEST_DISK_ID__"
 declare -r DEST_DISK_PATH="/dev/disk/by-id/${DEST_DISK_ID}"
 # <http://list.zfsonlinux.org/pipermail/zfs-discuss/2016-June/025765.html>
 # zpool now report full paths, rather than relative /dev paths to the disks which may or may not work properly with zfs. Grub utilities check zpool status for zfs pools to find the disks that contain them. Therefore changing the output of zpool status fixes grub.
-declare -r ZPOOL_VDEV_NAME_PATH=YES
+declare -x -r ZPOOL_VDEV_NAME_PATH=YES
 
-export PATH=/usr/sbin:$PATH
+export PATH=/usr/sbin:/sbin:$PATH
 
 ln -s /proc/self/mounts /etc/mtab
 apt-get update
